eb96eda3f723c7c6a67d7288925ae71b7a6a54e7
Amber Zimmerman <ajz1221@gmail.com>
2022-04-01 10:00:03 -0600

    updating category has many product statement

--- models/index.js
+++ models/index.js
@@ -4,14 +4,12 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Categories have many Products
+Category.hasMany(Product, {});
+
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-});
-
-// Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
